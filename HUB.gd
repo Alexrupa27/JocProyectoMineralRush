04edbcb0a0d77e953c5b43b09a125ac49890a245
Alexrupa27 <alexrupa123@gmail.com>
2024-10-29 20:13:29 +0100

    redimensionar

--- HUB.gd
+++ HUB.gd
@@ -5,11 +5,11 @@ var urlDiscord = "https://discord.gg/4U57ehVFQz"
 
 func _ready():
 	pass # Replace with function body.
-
-
-func _on_Button_pressed(): #boton Pagina Web
-	OS.shell_open(urlWeb)
-
+	
 
 func _on_DiscordButton_pressed():
 	OS.shell_open(urlDiscord)
+
+
+func _on_WebButton_pressed():
+	OS.shell_open(urlWeb)
